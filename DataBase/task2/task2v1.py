f128a0fd8a4126088f3dca345c8add8e4524c56d
vpironski <vpironski@gmail.com>
2022-10-23 22:29:14 +0300

    test

--- DataBase/task2/task2v1.py
+++ DataBase/task2/task2v1.py
@@ -1,6 +1,9 @@
+from insert_v_1 import INSERT
+
 choice = input("What do you want to do (SELECT or INSERT): ")
 choice = choice.lower()
 
 if choice == "insert":
-    from insert_v_1 import INSERT
     s = INSERT()
+
+

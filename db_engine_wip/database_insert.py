9c6e18651c6f3b6dad283c344e147d43957d83b2
gocheto8 <oreshkov236@gmail.com>
2022-10-31 16:33:39 +0200

    finished

--- db_engine_wip/database_insert.py
+++ db_engine_wip/database_insert.py
@@ -1,21 +1,40 @@
 import database_core
+import re
+from datetime import datetime
 
 
-def insert(database: database_core.Database):
+def insert(database: database_core.Database, values: list):
+    errors = list(check_lengths(database, values))
+    if not len(errors) == 0:
+        return errors
+    else:
+        # 'a' stands for append
+        with open(database.path, 'a', encoding='utf-8') as db_file:
+            for value, length in zip(values, database.colons.values()):
+                db_file.write(value)
+                if type(length) is int:
+                    for i in range(length - len(value)):
+                        db_file.write('~')
+            db_file.close()
+        return 'Success!'
 
-    # 'a' stands for append
-    with open(database.path, 'a', encoding='utf-8') as db_file:
 
-        db_file.close()
-
-
-def check_lengths(database: database_core.Database, values: list):
-    if not len(values) == len(database.colons):
-        # print('fail!')
-        return False
-
-    index = 0
-    for colon in database.colons:
-
-        index += 1
-        pass
+def check_lengths(database: database_core.Database, input_list: list):
+    if not len(input_list) == len(database.colons):
+        print(input_list, len(input_list), len(database.colons))
+        yield 'Wrong number of arguments!'
+    else:
+        for colon, value in zip(database.colons, input_list):
+            length_or_type = database.colons.get(colon)
+            if length_or_type not in database_core.types_length:
+                if len(value) > length_or_type:
+                    yield f'Value at {colon} is too long!'
+            else:
+                if length_or_type == 'date':
+                    try:
+                        datetime.strptime(value, '%Y-%m-%d')
+                    except ValueError:
+                        yield ValueError(f'Incorrect data format at {colon}, should be YYYY-MM-DD')
+                elif length_or_type == 'gender':
+                    if re.fullmatch(r'[mfn]', value):
+                        yield f'Incorrect value at {colon}, should be m/f/n'

9bdafa3361c87f9b89850d74700bc530d9634bbd
gocheto8 <oreshkov236@gmail.com>
2023-01-21 19:02:46 +0200

    tests

--- Engine/Tests/Testing.py
+++ Engine/Tests/Testing.py
@@ -1,24 +1,29 @@
-import os
+import Engine.Main.db_engine_main as eng
+"""
+missing:
+shrink, regex search (like)
 
-import Engine.db_engine_main as eng
+"""
 
-table_structure = [('first_name', 's', 5), ('age', 'h', 1)]
+# database1 = eng.Database('test')
+# util.linear_select(database1.tables['test_table01'])
 
-os.chdir('..')
-# eng.Database.create('test')
-# eng.Table.create_table('test', 'test_table01', table_structure)
-database1 = eng.Database('test')
-# print(database1.tables['test_table01'].current_entry_count)
-# entry_a = b'\x00\x00\x00\x00\x00\x00\x00\x64\x00\x00\x00\x00\x00\x00\x00\x00gamer'
-entry_b = [100, 0, 'gamer', 16]      # limit is 32 767
-# translated_entry_b = database1.tables.get('test_table01').translator.to_bin(entry_b)
-# print(translated_entry_b, type(translated_entry_b))
-database1.tables['test_table01'].write(0, entry_b)
-print(database1.tables['test_table01'].read(0))
-print(database1.tables['test_table01'].current_entry_count)
+db = eng.Database('generated')
 
-# try except block for translator usage
-# try:
-#     print(database1.tables['test_table01'].translator.from_bin(1234567))
-# except TypeError:
-#     print('wrong datatypes')
+# count
+# print(db.tables['worker'].current_entry_count)
+
+# create, delete
+# eng.Database.create('test1')
+# db.drop_schema()
+#  table creates & inserts are in generate
+
+columns = ['id', 'first_name', 'salary']
+for worker in db.linear_select('worker',
+                               start=0,
+                               limit=500,
+                               columns=columns,
+                               condition='salary > float(90000)'):
+    print(worker)
+    pass
+

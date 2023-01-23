d79d76fd8c2106cbc83524284868709e903bc3b8
gocheto8 <oreshkov236@gmail.com>
2023-01-23 21:27:58 +0200

    rnd

--- db_engine_new/Engine/engine_demo.py
+++ db_engine_new/Engine/engine_demo.py
@@ -1,2 +1,28 @@
+import os
+
 import db_engine_new.Engine.Main.db_engine_main as eng
 import tabulate
+
+
+def input_choice(options: tuple):
+    for i, item in enumerate(options):
+        print("{}. {}".format(i + 1, item))
+    print("Please choose one of the above options:")
+    while True:
+        choice = input()
+        if choice.isnumeric() and 1 <= int(choice) <= len(options):
+            break
+        else:
+            print("Invalid choice. Please try again.")
+    return int(choice)
+
+
+# first_action = input_choice(('USE', 'CREATE', 'DROP', 'QUIT'))
+# if first_action == 1:
+#     databases = os.listdir('Databases/')
+#
+data = [['apple', 20, '400'],
+        ['banana', 15, '120'],
+        ['potato', 30, '60'],
+        ]
+print(tabulate.tabulate(data, tablefmt=tabulate.tabulate_formats[1], headers=['name', 'weight', 'price']))
